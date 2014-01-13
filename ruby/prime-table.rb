159a4ed20e20aef8f2645f0ac3d67b88f28b4f6e
Jonny Frey <j0nn9.fr39@gmail.com>
2014-01-13 01:52:19 +0100

    added prime table

---
+++ ruby/prime-table.rb
@@ -0,0 +1,54 @@
+##
+# implementation of the default Sieve of Eratosthenes to generat 
+# a prime table including all primes till a given number starting by 2
+#
+class PrimeTable
+
+  def initialize sieve_size
+
+    @primes = []
+    
+    sieve = Array.new sieve_size, true
+
+    
+    # 0 and 1 ar not primes 
+    sieve[0] = false;
+    sieve[0] = false;
+ 
+    # run the sieve 
+    for i in (2..Math.sqrt(sieve_size)) do 
+
+      if sieve[i] == true
+        for p in ((i * i)..sieve_size) do
+          sieve[p] = false
+        end
+      end
+    end
+
+    # save the primes
+    for i in (2..sieve_size) do 
+      
+      if sieve[i] == true
+        @primes << i
+      end
+    end
+
+    # no longer needed
+    sieve.clear
+  end
+
+  ##
+  # access the prime at index i
+  #
+  def [](i)
+    return @prime[i]
+  end
+
+  ##
+  # return the total number of primes
+  #
+  def num_primes
+    return @primes.length
+  end
+
+end
