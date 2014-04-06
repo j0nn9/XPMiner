654a51775c808c993320748ebe7361be472d163c
Jonny Frey <j0nn9.fr39@gmail.com>
2014-04-06 23:13:15 +0200

    fixed smal issues and improoved readability in block

--- c/src/block.c
+++ c/src/block.c
@@ -4,6 +4,7 @@
  * (new transactions are saved into a block,
  *  but for poolmining we only need the block header)
  */
+#include <time.h>
 #include <stdio.h>
 #include <gmp.h>
 
@@ -14,10 +15,10 @@
  */
 void header_set_null(BlockHeader *header) {
   
-  header->version = BLOCK_HEADER_VERSION;
-  header->time    = 0;
-  header->min_difficulty    = 0;
-  header->nonce   = 0;
+  header->version    = BLOCK_HEADER_VERSION;
+  header->time       = 0;
+  header->difficulty = 0;
+  header->nonce      = 0;
 
   memset(header->hash_prev_block,  0, HASH_LENGTH);
   memset(header->hash_merkle_root, 0, HASH_LENGTH);
@@ -38,7 +39,7 @@ void get_header_hash(BlockHeader *header,
   SHA256_Update(&sha256, header->hash_prev_block, HASH_LENGTH);                                   
   SHA256_Update(&sha256, header->hash_merkle_root, HASH_LENGTH);                                   
   SHA256_Update(&sha256, &header->time, 4);                                   
-  SHA256_Update(&sha256, &header->min_difficulty, 4);                                   
+  SHA256_Update(&sha256, &header->difficulty, 4);                                   
   SHA256_Update(&sha256, &header->nonce, 4);                                   
   SHA256_Final(tmp, &sha256); 
  
@@ -49,48 +50,57 @@ void get_header_hash(BlockHeader *header,
 }
 
 /**
- * converts the given data to a BlockHeader
+ * sets the time value for the given block header
+ * (each thread gets a different time, so they
+ *  don't use the same header hash for mining)
  */
-void convert_data_to_header(uint8_t data[BLOCK_HEADER_LENGTH], 
-                            BlockHeader *header) {
+void header_set_time(BlockHeader *header, 
+                     uint32_t n_threads, 
+                     uint32_t cur_thread) {
 
-  header->version = *((uint32_t *) data);
-
-  memcpy(header->hash_prev_block,  data + 4, HASH_LENGTH);
-  memcpy(header->hash_merkle_root, data + 4 + HASH_LENGTH, HASH_LENGTH);
-
-  header->time           = *((uint32_t *) (data + 4  + HASH_LENGTH * 2));
-  header->min_difficulty = *((uint32_t *) (data + 8  + HASH_LENGTH * 2));
-  header->nonce          = *((uint32_t *) (data + 12 + HASH_LENGTH * 2));
-
-  memset(header->primemultiplier, 0, MULTIPLIER_LENGTH);
+  
+  header->time  = opts.time_offset;
+  header->time += ((time(NULL) + n_threads) / n_threads) * n_threads;
+  header->time += cur_thread;
 }
 
 /**
- * modifoy the block header (by increasing the nonce value) 
+ * modify the block header (by increasing the nonce value) 
  * to have an hash divisibel by the first n primes
  * (where n is --nprimesinhash)
  *
  * the higher n is the more mining becomes like bitoin
  * (seraching for a specific sha256 hash)
+ * on the other side a highly composite hash improoves
+ * seraching for prime chains
  */
 void mine_header_hash(Sieve *sieve, uint32_t n_threads) {
 
   uint8_t hash[SHA256_DIGEST_LENGTH];
+  uint_fast8_t reminder = 1;
 
-  /**
-   * mine for a heash
-   */
+  /* mine for a hash */
   do {
-    
-    /* change the hash (make sure each thread has a different hash) */
-    sieve->header.nonce += n_threads;
-    
-    get_header_hash(&sieve->header, hash);
-    mpz_set_sha256(sieve->mpz_hash, hash);
 
-    /* reminder = hash % hash_primorial */
-    mpz_mod(sieve->mpz_reminder, sieve->mpz_hash, sieve->mpz_hash_primorial);
+    /* all nonce values used ? */
+    if (sieve->header.nonce == UINT32_MAX) {
+
+      /* addjust time */
+      sieve->header.time += n_threads;
+    }
     
-  } while (running && mpz_cmp_ui(sieve->mpz_reminder, 0));
+    /* change the hash */
+    sieve->header.nonce++;
+
+    get_header_hash(&sieve->header, hash);
+    
+    /* skip check if hash is smaler than 2^255 */
+    if (hash[SHA256_DIGEST_LENGTH - 1] & 0x80) {
+
+      mpz_set_sha256(sieve->mpz_hash, hash);
+ 
+      /* reminder = hash % hash_primorial */
+      reminder = mpz_tdiv_ui(sieve->mpz_hash, opts.hash_primorial);
+    }
+  } while (running && reminder);
 }
--- c/src/block.h
+++ c/src/block.h
@@ -17,7 +17,7 @@
 
 /**
  * The current header version
- * (number form originla client)
+ * (number form original client)
  */
 #define BLOCK_HEADER_VERSION 2
 
@@ -29,7 +29,7 @@
 /**
  * the byte length of the prime multiplier
  */
-#define MULTIPLIER_LENGTH 48
+#define MULTIPLIER_LENGTH 47
 
 /**
  * the block header length
@@ -37,19 +37,22 @@
 #define BLOCK_HEADER_LENGTH 128
 
 /**
- * the block header 
+ * the block header
+ * (using packed so gcc don't optimizes the size)
  */
 struct BlockHeader {
-  // comments: BYTES <index> + <length>
-  int32_t       version;                              // 0+4
-  uint8_t       hash_prev_block[HASH_LENGTH];         // 4+32
-  uint8_t       hash_merkle_root[HASH_LENGTH];        // 36+32
-  uint32_t      time;                                 // 68+4
-  uint32_t      min_difficulty;                       // 72+4
-  /* value just to chainge the hash */
-  uint32_t      nonce;                                // 76+4
-  uint8_t       primemultiplier[MULTIPLIER_LENGTH];   // 80+48
-};  // =128 bytes header (80 default + 48 primemultiplier)
+  int32_t       version;                             
+  uint8_t       hash_prev_block[HASH_LENGTH];       
+  uint8_t       hash_merkle_root[HASH_LENGTH];     
+  uint32_t      time;                             
+  uint32_t      difficulty;                  
+
+  /* value just to chainge the header hash */
+  uint32_t      nonce;                               
+
+  uint8_t       multiplier_length;
+  uint8_t       primemultiplier[MULTIPLIER_LENGTH]; 
+} __attribute__ ((packed)); 
 
 /**
  * set a block header to zero
@@ -74,23 +77,26 @@ void get_header_hash(BlockHeader *header, uint8_t hash[SHA256_DIGEST_LENGTH]);
 /**
  * converts an mpz value to an 48 byte array
  */
-#define mpz_to_ary(mpz_origin, ary) \
-  mpz_import(ary, HASH_LENGTH, -1, sizeof(ary[0]), -1, 0, mpz_origin)
+#define mpz_to_ary(mpz_origin, ary, len) \
+  mpz_export(ary, len, -1, sizeof(ary[0]), -1, 0, mpz_origin)
 
 
 /**
- * converts the given data to a BlockHeader
+ * sets the time value for the given block header
  */
-void convert_data_to_header(uint8_t data[BLOCK_HEADER_LENGTH], 
-                            BlockHeader *header);
+void header_set_time(BlockHeader *header, 
+                     uint32_t n_threads, 
+                     uint32_t cur_thread);
 
 /**
- * modifoy the block header (by increasing the nonce value) 
+ * modify the block header (by increasing the nonce value) 
  * to have an hash divisibel by the first n primes
  * (where n is --nprimesinhash)
  *
  * the higher n is the more mining becomes like bitoin
  * (seraching for a specific sha256 hash)
+ * on the other side a highly composite hash improoves
+ * seraching for prime chains
  */
 void mine_header_hash(Sieve *sieve, uint32_t n_threads);
  
