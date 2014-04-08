a4d07aa8dc3d69949dc89400252ba8ef3d00baa7
Jonny Frey <j0nn9.fr39@gmail.com>
2014-04-09 01:01:29 +0200

    added GPLv3 license

--- src/block.h
+++ src/block.h
@@ -1,8 +1,23 @@
 /**
- * impelmentaion of the primecoin block header
+ * Impelmentaion of the primecoin block header.
  *
  * (new transactions are saved into a block,
- *  but for poolmining we only need the block header)
+ *  but for pool mining we only need the block header)
+ *
+ * Copyright (C)  2014  Jonny Frey  <j0nn9.fr39@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef __BLOCK_H__
 #define __BLOCK_H__
--- src/prime-table.c
+++ src/prime-table.c
@@ -1,6 +1,21 @@
 /**
- * implementation of the default Sieve of Eratosthenes to generat 
- * a prime table including all primes till a given number starting by 2
+ * Implementation of the default Sieve of Eratosthenes to generat 
+ * a prime table including all primes till a given number starting by 2.
+ *
+ * Copyright (C)  2014  Jonny Frey  <j0nn9.fr39@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <stdlib.h>
 #include <stdio.h>
